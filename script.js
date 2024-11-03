4e566c7ccae3a7b8790d1d58f1408b27a43ae955
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-03 12:31:58 +0300

    Add playRound()
    It compares computer and human choices and decides
    who is a winner

--- script.js
+++ script.js
@@ -33,3 +33,36 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  // Compare choices of computer and human 
+  // Print round winner
+  // Increment winner score
+  if (humanChoice === 'rock' && computerChoice === 'scissors') {
+    console.log('You win! Rock beats Scissors!');
+    humanScore++;
+  } else if (humanChoice === 'rock' && computerChoice === 'paper') {
+    console.log('You lose! Paper beats Rock!');
+    computerScore++;
+  } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+    console.log('You win! Paper beats Rock!');
+    humanScore++;
+  } else if (humanChoice === 'paper' && computerChoice === 'scissors') {
+    console.log('You lose! Scissors beat paper!');
+    computerScore++;
+  } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+    console.log('You win! Scissors beat Paper!');
+    humanScore++;
+  } else if (humanChoice === 'scissors' && computerChoice === 'rock') {
+    console.log('You lose! Rock beats Scissors!');
+    computerScore++
+  } else if (humanChoice === computerChoice) {
+    console.log("It's a tie!");
+    humanScore++;
+    computerScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection, computerSelection);

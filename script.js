e952c13a5071fe9cdcb04339736fa534b1c663ce
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2024-11-03 11:17:53 +0300

    Add getComputerChoice()
    It randomly puts 'rock' or 'paper' or 'scissors'
    in console.log()

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function getComputerChoice() {
+  let result = Math.floor((Math.random() * 3) + 1)
+  switch (result) {
+    case 1:
+      return 'rock';
+      break;
+    case 2:
+      return 'paper';
+      break;
+    case 3:
+      return 'scissors';
+      break;
+  }
+}
